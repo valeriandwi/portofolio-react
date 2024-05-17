134e1ab690404e2181d09c84a9d08ebb728e3241
Valerian Dwi Purnomo <valerian.purnomo@inticorp.tech>
2024-05-17 23:59:56 +0700

    Fixing mobile version

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -15,28 +15,34 @@ import {
 const MainPage: React.FC = () => {
   return (
     <div>
-      <p className="top-[2rem] left-[2rem] fixed text-white z-[999]">
-        <AiFillAliwangwang size="40" color="#fff" />
-      </p>
-      <p className="text-white z-[999] block bottom-[5rem] left-[2rem] fixed">
-        <div className="flex gap-8 flex-col">
-          <AiFillInstagram size={"40"} color="#71717A" />
-          <AiFillGithub size={"40"} color="#71717A" />
-          <AiFillLinkedin size={"40"} color="#71717A" />
-        </div>
-      </p>
+      <div className="max-lg:hidden">
+        <p className="top-[2rem] left-[2rem] fixed text-white z-[999]">
+          <AiFillAliwangwang size="40" color="#fff" />
+        </p>
+        <p className="text-white z-[999] block bottom-[5rem] left-[2rem] fixed">
+          <div className="flex gap-8 flex-col">
+            <AiFillInstagram size={"40"} color="#71717A" />
+            <AiFillGithub size={"40"} color="#71717A" />
+            <AiFillLinkedin size={"40"} color="#71717A" />
+          </div>
+        </p>
+      </div>
 
       <div className="max-w-screen h-fit">
-        <div className="gradient-bg h-[150vh]">
-          <div className="flex-col flex space-y-[60vh]">
-            <section className="text-container justify-center pl-[10rem]">
-              <h2 className="text-6xl">Hi, My Name is</h2>
-              <h1 className="text-8xl font-bold">Valerian Dwi Purnomo</h1>
-              <h3 className="text-2xl">Frontend Web Developer</h3>
+        <div className="gradient-bg h-[150vh] max-lg:h-[200vh]">
+          <div className="flex-col flex space-y-[65vh] max-lg:space-y-[100vh]">
+            <section className="text-container justify-center lg:pl-[10rem] max-lg:pl-[2rem]">
+              <h2 className="text-6xl max-lg:text-4xl">Hi, My Name is</h2>
+              <h1 className="text-8xl font-bold max-lg:text-6xl">
+                Valerian Dwi Purnomo
+              </h1>
+              <h3 className="text-2xl max-lg:text-2xl">
+                Frontend Web Developer
+              </h3>
             </section>
-            <section className="text-container justify-center pl-[10rem] pr-[12rem]">
-              <h2 className="text-2xl">About Me</h2>
-              <h3 className="text-6xl font-medium">
+            <section className="text-container justify-center lg:pl-[10rem] pr-[12rem] max-lg:pl-[2rem] max-lg:pr-[4rem]">
+              <h2 className="text-2xl max-lg:text-2xl">About Me</h2>
+              <h3 className="text-6xl font-medium max-lg:text-4xl">
                 Constantly pushing the boundaries of what's possible, I love
                 diving into the latest technologies
               </h3>
@@ -51,9 +57,9 @@ const MainPage: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className="h-fit py-[5rem] w-full relative bg-[#101013] pl-[10rem] text-white pr-[12rem]">
+      <div className="h-fit py-[5rem] w-full relative bg-[#101013] pl-[10rem] max-lg:pl-[2rem] text-white pr-[12rem] max-lg:pr-[4rem]">
         <h2 className="text-2xl">Experience</h2>
-        <h3 className="text-4xl font-medium">
+        <h3 className="text-4xl font-medium max-lg:text-2xl">
           As a frontend web developer with over 4 years of experience, I have
           successfully contributed to various web development projects,
           possessing a deep understanding of modern technologies, especially
