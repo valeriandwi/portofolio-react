e70e939e0c7cc071a35cbe1fa4e23c64d9ea3d0e
Valerian <valerian.purnomo@inticorp.tech>
2024-05-20 16:09:49 +0700

    Fixing basepath

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
-    base: "./portofolio-react/",
+    base: "./",
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src/"),
