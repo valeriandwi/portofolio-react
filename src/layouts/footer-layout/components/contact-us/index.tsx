c306ccbaee749ff3f87ba746b85947cbf2440e06
Valerian <valerian.purnomo@inticorp.tech>
2024-05-20 16:45:08 +0700

    Fixing tailwind

--- src/layouts/footer-layout/components/contact-us/index.tsx
+++ src/layouts/footer-layout/components/contact-us/index.tsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ContactUs: React.FC = () => {
   return (
-    <div className="w-full absolute bg-black lg:pl-[10rem] pt-16 py-8 flex flex-col space-y-4">
-      <div className="text-white">
-        Email <br />
-        valerian.dwi.p@gmail.com
-      </div>
-      <div className="text-white">
-        Location <br />
-        Jakarta, Indonesia
+    <div className="w-full absolute bg-black">
+      <div className="pl-32 flex flex-col space-y-4 py-8 text-white">
+        <p>
+          Email <br />
+          valerian.dwi.p@gmail.com
+        </p>
+        <p>
+          Location <br />
+          Jakarta, Indonesia
+        </p>
+        <p>
+          LinkedIn <br />
+          <Link to="">https://</Link>
+        </p>
       </div>
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
     plugins: [react()],
-    base: "./portofolio-react",
+    base: env.VITE_UI_BASE_PATH || "./",
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src/"),
